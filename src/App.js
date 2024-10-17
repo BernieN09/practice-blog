d5a04c73dcb51762e32a45e1ffb06f758752f6af
Bernie <BernieDNnadi@gmail.com>
2024-10-17 15:55:16 -0500

    code refactoring, inlining methods

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Create from './Create';
-import BlogDetails from './BlogDetails';
-import NotFound from './NotFound';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home/>}>
-          </Route>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
 
-          <Route path="/create" element={<Create/>}>
-          </Route>
+            <Route path="/create" element={<Create />}></Route>
 
-          <Route path="/blogs/:id" element={<BlogDetails />}>
-          </Route>
+            <Route path="/blogs/:id" element={<BlogDetails />}></Route>
 
-          <Route path="*" element={<NotFound />}>
-          </Route>
-        </Routes>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
